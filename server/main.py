ee4ab85a8b8a7929e811012a31e1e691e7e59017
Nihaar-E <edulanihaarreddy16.com>
2025-01-22 17:32:11 +0530

    initial commit

--- server/main.py
+++ server/main.py
@@ -5,8 +5,8 @@ from io import BytesIO
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-#from dotenv import load_dotenv
-#load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
 
@@ -18,22 +18,42 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers = ["*"],
 )
+s3 = boto3.client(
+    's3',
+    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
 
-@app.post("/generate-qr/")  
-async def generate_qr(url:str):
+bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+
+@app.post("/generate-qr/")
+async def generate_qr(url: str):
+    # Generate QR Code
     qr = qrcode.QRCode(
-        version = 1,
-        error_correction = qrcode.constants.ERROR_CORRECT_L,
-        box_size = 10,
-        border = 4,
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
     )
     qr.add_data(url)
-    qr.make(fit= True)
+    qr.make(fit=True)
 
-    img = qr.make_image( fill_color = "black", back_color = "white")
-
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    # Save QR Code to BytesIO object
     img_byte_arr = BytesIO()
-    img.save(img_byte_arr, format = 'PNG')
+    img.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
 
-    return StreamingResponse(img_byte_arr, media_type="image/png")
+    # Generate file name for S3
+    file_name = f"qr_codes/{url.split('//')[-1]}.png"
+
+    try:
+        # Uploading to S3
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        
+        # Generate the S3 URL
+        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+        return {"qr_code_url": s3_url}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
